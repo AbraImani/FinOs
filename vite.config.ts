2e0512b3e00df42df1fd446788d4db45c28498fb
Abraham Imani <abrahamfaith325@gmail.com>
2026-02-15 22:16:08 +0200

    Ajout de la fonctionnalité offline et des icones

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.svg', 'icons/*.png'],
+      includeAssets: ['favicon.svg', 'icons/*.svg'],
       manifest: {
         name: 'FinOS - Personal Finance Manager',
         short_name: 'FinOS',
@@ -18,9 +18,9 @@ export default defineConfig({
         scope: '/',
         start_url: '/',
         icons: [
-          { src: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
-          { src: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' },
+          { src: '/icons/icon-192x192.svg', sizes: '192x192', type: 'image/svg+xml' },
+          { src: '/icons/icon-512x512.svg', sizes: '512x512', type: 'image/svg+xml' },
+          { src: '/icons/icon-512x512.svg', sizes: '512x512', type: 'image/svg+xml', purpose: 'maskable' },
         ],
       },
       workbox: {
